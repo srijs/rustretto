91364942d017608ef5193e8174e2d7a2e1f92ba0
Sam Reis <sreis@atlassian.com>
2018-11-16 15:46:39 +1100

    export Object::init stub from runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,11 +1,13 @@
 extern crate libc;
 
-mod unwind;
+use libc::c_void;
+//mod unwind;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[repr(C)]
+pub struct Ref {
+    object: *const c_void,
+    vtable: *const c_void,
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn _Jm_java_lang_Object__init(_this: Ref) {}
