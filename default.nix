1e75e87672d6734483534672f71efa2025573f19
Sam Rijs <srijs@airpost.net>
2018-12-11 21:47:59 +1100

    introduce travis checks

--- default.nix
+++ default.nix
@@ -1,16 +1,14 @@
 with import <nixpkgs> {};
 
-let
-  frameworks = darwin.apple_sdk.frameworks;
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   name = "env";
 
   env = buildEnv { name = name; paths = buildInputs; };
 
-  buildInputs = [
-    frameworks.Security
+  platformBuildInputs =
+    if hostPlatform.isDarwin
+    then [ darwin.apple_sdk.frameworks.Security ]
+    else [ ];
 
-    libxml2
-    llvm_7
-  ];
+  buildInputs = platformBuildInputs ++ [ libxml2 llvm_7 ];
 }
