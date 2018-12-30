516a46a45aa11799192968790c31b55c801f895f
Sam Rijs <srijs@airpost.net>
2018-12-30 12:50:01 +1100

    upgrade llvm-sys to 70.0.2

--- crates/llvm/build.rs
+++ crates/llvm/build.rs
@@ -2,11 +2,6 @@ fn main() {
     println!("cargo:rustc-link-lib=LLVM-7");
 
     cc::Build::new()
-        .file("wrappers/target.c")
-        .opt_level(3)
-        .compile("targetwrappers");
-
-    cc::Build::new()
         .file("wrappers/triple.cpp")
         .opt_level(3)
         .cpp(true)
